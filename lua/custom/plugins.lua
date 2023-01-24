a210adeb6ecf6e216dceae6ea086fee6d3f14aef
Juan Ochoa <ochovilla@gmail.com>
2023-01-24 05:43:05 -0800

    Using kickstart.vim for a complete, but sensible config
    - lsp servers (pylsp, lua, emmet_ls, tsserver, svelte, rust)

---
+++ lua/custom/plugins.lua
@@ -0,0 +1,15 @@
+-- NOTE:
+-- from the kickstart.vim README:
+-- .. create a separate custom.plugins module to
+-- register your own plugins. In addition, you can
+-- handle further customizations in
+-- the /after/plugin/ directory
+
+return function(use)
+--  use({
+--    "folke/which-key.nvim",
+--      config = function()
+--        require("which-key").setup({})
+--      end
+--  })
+end
